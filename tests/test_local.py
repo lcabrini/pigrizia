8de44722c3acdec4b9ae13e27393e46a87d249a0
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-19 00:12:15 +0000

    Added tests for file_exists and directory exists.

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,6 +1,8 @@
 import unittest
 from pigrizia.host.linux import Linux
 
+# TODO: this should change name, since the tests are on a local Linux
+# system.
 class LocalTest(unittest.TestCase):
     def setUp(self):
         pass
@@ -8,8 +10,15 @@ class LocalTest(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_foo(self):
+    def test_file_exists(self):
         host = Linux()
+        self.assertTrue(host.file_exists('/etc/hosts'))
+        self.assertFalse(host.file_exists('/etc/non-existent'))
+
+    def test_directory_exists(self):
+        host = Linux()
+        self.assertTrue(host.directory_exists('/etc'))
+        self.assertFalse(host.directory_exists('/non-existent'))
 
 if __name__ == '__main__':
     unittest.main()
