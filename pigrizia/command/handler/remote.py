df94ab4dca5366c17e812b6528865177f6ebbab1
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-19 01:26:26 +0000

    Don't require username and password to remote handler.

--- pigrizia/command/handler/remote.py
+++ pigrizia/command/handler/remote.py
@@ -1,3 +1,4 @@
+from getpass import getuser
 import paramiko
 
 class RemoteHandler:
@@ -9,9 +10,25 @@ class RemoteHandler:
     :param str passwd: the password assoctiated with the user
     """
 
-    def __init__(self, user, passwd, **kwargs):
-        self.user = user
-        self.passwd = passwd
+    def __init__(self, **kwargs):
+        if 'user' in kwargs:
+            self.user = kwargs['user']
+        else:
+            self.user = getuser()
+
+        if 'passwd' in kwargs:
+            self.passwd = kwargs['passwd']
+        else:
+            # For now we don't do anything, since it is fully possible
+            # that the user doesn't need a password (key-based SSH). But
+            # we'll keep the else here since I am sure to have forgot
+            # something.
+            pass
+
+        if 'passphrase' in kwargs:
+            # TODO: is this needed? Since I don't know, I will keep it 
+            # here for now.
+            self.passphrase = kwargs['passphrase']
 
     def do(self, cmd, **kwargs):
         pass
