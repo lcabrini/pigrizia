d2b18726a8075a6be4d7f0283280de42cc1d7601
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-20 18:57:21 +0000

    Fix in setup.py.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         packages=setuptools.find_packages(),
         install_requires=[
             'paramiko',
-            ]
+            ],
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
