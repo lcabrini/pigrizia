b387389b1d0b1cdfaba4b8e3c9e60937b276d7b9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2019-12-21 22:20:03 +0000

    Added placeholder class for Windows, without any functionality.

---
+++ pigrizia/host/windows.py
@@ -0,0 +1,11 @@
+from . import Host
+
+class Windows(Host):
+    """
+    Represents a Windows host.
+
+    This is currently not implemented and it is likely to get implemented
+    without some help from somebody who knows Windows.
+    """
+    def __init__(self, **kwargs):
+        raise NotImplementedError("Help needed.")
